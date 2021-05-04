97d8257f1eda0d6d3a1503342fcc67e030618ab3
candrepa1 <candrepa1@gmail.com>
2021-05-04 08:13:14 -0500

    form with select input has been created for the user to choose how many dice they want to roll

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container } from "./App.styled";
 
 function App() {
-	return <div className="App"></div>;
+	const arr = [...Array(10).keys()].map((x) => x + 1);
+	const [numberOfDice, setNumberOfDice] = useState(1);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setNumberOfDice(Number(e.target.numOfDice.value));
+	};
+
+	return (
+		<Container>
+			<form
+				style={{ color: "white", marginBottom: "20px" }}
+				onSubmit={handleSubmit}
+			>
+				<label>
+					Dice to be rolled:
+					<select name="numOfDice">
+						{arr.map((element) => (
+							<option key={element} value={element}>
+								{element}
+							</option>
+						))}
+					</select>
+				</label>
+				<button type="submit">Submit</button>
+			</form>
+		</Container>
+	);
 }
 
 export default App;
